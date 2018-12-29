8ce9d2ac794f84083ce15b01fbe8dee994756bcd
Alex Burck <me@alexburck.com>
2018-12-29 00:00:50 -0600

    ad basic update script

--- sql-workbench/tools/chocolateyInstall.ps1
+++ sql-workbench/tools/chocolateyInstall.ps1
@@ -1,20 +1,24 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 # DO NOT CHANGE THESE MANUALLY. USE update.ps1
-$url      = 'https://www.sql-workbench.eu/Workbench-Build124.zip'
-$checksum = '3a54f1293524ec792e9abd1c57c7c0509ab42ae914a46bee36d7a1a12abb4603'
+$url      = 'https://www.sql-workbench.eu/Workbench-Build123.zip'
+$checksum = 'ABC'
 
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $sqlWorkbench = Join-Path $toolsDir 'SQLWorkbench.exe'
 $sqlWorkbench64 = Join-Path $toolsDir 'SQLWorkbench64.exe'
+$sqlwbconsole = Join-Path $toolsDir 'sqlwbconsole.exe'
+$sqlwbconsole64 = Join-Path $toolsDir 'sqlwbconsole64.exe'
 
 # Create .ignore and .gui files based on OS
 if (Get-ProcessorBits -Compare 64) {
   New-Item "$sqlWorkbench.ignore" -Type File -Force | Out-Null
+  New-Item "$sqlwbconsole.ignore" -Type File -Force | Out-Null
   New-Item "$sqlWorkbench64.gui" -Type File -Force | Out-Null
 } else {
   New-Item "$sqlWorkbench64.ignore" -Type File -Force | Out-Null
+  New-Item "$sqlwbconsole64.ignore" -Type File -Force | Out-Null
   New-Item "$sqlWorkbench.gui" -Type File -Force | Out-Null
 }
 
@@ -31,4 +35,5 @@ Install-ChocolateyZipPackage @packageArgs
 # If this is a 64 bit OS, create a secondary shim
 if (Get-ProcessorBits -Compare 64) {
   Install-BinFile -Name 'SQLWorkbench' -Path $sqlWorkbench64 -UseStart
+  Install-BinFile -Name 'sqlwbconsole' -Path $sqlwbconsole64
 }
--- sql-workbench/update.ps1
+++ sql-workbench/update.ps1
@@ -23,23 +23,12 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-  $response = Invoke-WebRequest -Uri $razerSynapseUrl -UseBasicParsing -MaximumRedirection 0 -ErrorAction Ignore
-  if ($response.StatusCode -ne 302) {
-    throw "HTTP $($response.StatusCode) when requesting $razerSynapseUrl"
-  }
-  if (-not $response.Headers.Location) {
-    throw "No Location header returned when requesting $razerSynapseUrl"
-  }
-
-  $downloadUrl = $response.Headers.Location
-  if (-not ($downloadUrl -match '.*_v(?<Version>.*)\.exe')) {
-    throw "Could not determine version from url $downloadUrl"
-  }
-  $version = $matches.Version
+  # TODO
+  $build = '124'
 
   return @{
-    Version = $version
-    URL32   = $downloadUrl
+    Version = "$build.0.0"
+    URL32   = "https://www.sql-workbench.eu/Workbench-Build$($build).zip"
   }
 }
 
