1d18eaadba3d52447c5d685197fc01b7893daf61
Alex Burck <me@alexburck.com>
2018-12-28 23:28:21 -0600

    setup install script

--- sql-workbench/tools/chocolateyInstall.ps1
+++ sql-workbench/tools/chocolateyInstall.ps1
@@ -1,18 +1,34 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 # DO NOT CHANGE THESE MANUALLY. USE update.ps1
-$url        = 'http://dl.razerzone.com/drivers/Synapse2/win/Razer_Synapse_Installer_v2.21.21.1.exe'
-$checksum   = '40a8baca006828d8dd73ba2be98e0eb244a5227b1a5661108153889d14540cfd'
+$url      = 'https://www.sql-workbench.eu/Workbench-Build124.zip'
+$checksum = '3a54f1293524ec792e9abd1c57c7c0509ab42ae914a46bee36d7a1a12abb4603'
+
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$sqlWorkbench = Join-Path $toolsDir 'SQLWorkbench.exe'
+$sqlWorkbench64 = Join-Path $toolsDir 'SQLWorkbench64.exe'
+
+# Create .ignore and .gui files based on OS
+if (Get-ProcessorBits -Compare 64) {
+  New-Item "$sqlWorkbench.ignore" -Type File -Force | Out-Null
+  New-Item "$sqlWorkbench64.gui" -Type File -Force | Out-Null
+} else {
+  New-Item "$sqlWorkbench64.ignore" -Type File -Force | Out-Null
+  New-Item "$sqlWorkbench.gui" -Type File -Force | Out-Null
+}
 
 $packageArgs = @{
   packageName            = $env:ChocolateyPackageName
-  fileType               = 'EXE'
+  unzipLocation          = $toolsDir
   url                    = $url
   checksum               = $checksum
   checksumType           = 'sha256'
-  softwareName           = 'Razer Synapse'
-  silentArgs             = "/S /v/qn"
-  validExitCodes         = @(0, 3010, 1641)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyZipPackage @packageArgs
+
+# If this is a 64 bit OS, create a secondary shim
+if (Get-ProcessorBits -Compare 64) {
+  Install-BinFile -Name 'SQLWorkbench' -Path $sqlWorkbench64 -UseStart
+}
